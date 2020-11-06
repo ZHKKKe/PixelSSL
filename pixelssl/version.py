a7c76c9bb12240a297a7ed98178d149ee63ad942
ZHKKKe <kezhanghan@outlook.com>
2020-11-06 12:46:10 +0800

    fix typos

--- pixelssl/version.py
+++ pixelssl/version.py
@@ -14,6 +14,6 @@ __author_email__ = 'kezhanghan@outlook.com'
 __updates__ = {
     '0.1.3': 'supports the CCT semi-supervised learning algorithm for pixel-wise classification',
     '0.1.2': 'supports PSPNet and its SSL results for semantic segmentation task',
-    '0.1.1': 'v0.1.1 supports the S4L semi-supervised learning algorihm and fixes some bugs in the demo code of semantic segmentation task',
+    '0.1.1': 'supports the S4L semi-supervised learning algorihm and fixes some bugs in the demo code of semantic segmentation task',
     '0.1.0': 'supports supervised-only learning, three semi-supervised learning algorithms (MT, AdvSSL, GCT) and one example task (semantic segmentation)',
 }
